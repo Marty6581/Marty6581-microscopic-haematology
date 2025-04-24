92a12df990e7f6409b4eb49f8cf186dc514591c4
Marty6581 <72860711+Marty6581@users.noreply.github.com>
2025-04-24 10:41:24 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+        // Toggle hamburger menu
+        const hamburger = document.querySelector('.hamburger');
+        const navMenu = document.querySelector('.nav-menu');
+
+        hamburger.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const links = document.querySelectorAll(".nav-menu a");
+            const currentUrl = window.location.pathname.split("/").pop(); // Get the current file name
+
+            links.forEach(link => {
+                if (link.getAttribute("href") === currentUrl) {
+                    link.classList.add("active");
+                }
+            });
+        });
+
+
+        // Collapsable part for case studies sections 
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const headers = document.querySelectorAll('.toggle-header');
+        
+            headers.forEach(header => {
+                header.addEventListener('click', function () {
+                    const content = this.nextElementSibling;
+                    content.classList.toggle('expanded');
+                });
+            });
+        });
+
+
+
+  // Get the current year
+  const currentYear = new Date().getFullYear();
+  // Find the element with the ID 'copyright-year' and set its text content
+  document.getElementById('copyright-year').textContent = currentYear;
+
+  
+
+  // Contact form
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        const confirmation = document.getElementById("confirmation");
+        
+        fetch("/", {
+            method: "POST",
+            body: new FormData(form)
+        }).then(() => {
+            form.reset();
+            form.classList.add("cloak"); // was "hidden"
+            confirmation.classList.remove("cloak"); // was "hidden"
+        }).catch(error => console.error("Error submitting form", error));
+    }
+
